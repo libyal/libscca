de55c70c2d54d75fb364b457de5ce838abd6c92d
Joachim Metz <joachim.metz@gmail.com>
2015-06-13 14:05:36 +0200

    Fixes for missing libfwnt in synclibs scripts.

--- synclibs.ps1
+++ synclibs.ps1
@@ -3,7 +3,7 @@
 # Version: 20150105
 
 $GitUrlPrefix = "https://github.com/libyal"
-$LocalLibs = @("libbfio","libcdata","libcerror","libcfile","libclocale","libcnotify","libcpath","libcsplit","libcstring","libcsystem","libcthreads","libfcache","libfdata","libfdatetime","libfvalue","libuna")
+$LocalLibs = @("libbfio","libcdata","libcerror","libcfile","libclocale","libcnotify","libcpath","libcsplit","libcstring","libcsystem","libcthreads","libfcache","libfdata","libfdatetime","libfvalue","libfwnt","libuna")
 
 foreach (${LocalLib} in ${LocalLibs})
 {
