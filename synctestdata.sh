cb5845b90c7a283f3962af6120c9be4ee4b4400d
Joachim Metz <joachim.metz@gmail.com>
2016-10-23 08:54:08 +0200

    Worked on tests.

---
+++ synctestdata.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Script that synchronizes the local test data
+#
+# Version: 20161009
+
+TEST_SET="public";
+TEST_INPUT_DIRECTORY="tests/input";
+TEST_FILES="CMD.EXE-087B4001.pf PING.EXE-B29F6629.pf TASKHOST.EXE-3AE259FC.pf WUAUCLT.EXE-830BCC14.pf";
+
+mkdir -p "${TEST_INPUT_DIRECTORY}/${TEST_SET}";
+
+for TEST_FILE in ${TEST_FILES};
+do
+	URL="https://github.com/log2timeline/plaso/blob/master/test_data/${TEST_FILE}?raw=true";
+
+	curl -L -o "${TEST_INPUT_DIRECTORY}/${TEST_SET}/${TEST_FILE}" ${URL};
+done
+
