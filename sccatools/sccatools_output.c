a72202f71ae6e2d9b3eca10324d321a72f8dfba2
Joachim Metz <joachim.metz@gmail.com>
2019-01-13 15:49:47 +0100

    Applied updates

--- sccatools/sccatools_output.c
+++ sccatools/sccatools_output.c
@@ -1,7 +1,7 @@
 /*
  * Common output functions for the sccatools
  *
- * Copyright (C) 2011-2018, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2011-2019, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
@@ -101,7 +101,7 @@ void sccatools_output_copyright_fprint(
 	 */
 	fprintf(
 	 stream,
-	 _( "Copyright (C) 2011-2018, %s.\n" ),
+	 _( "Copyright (C) 2011-2019, %s.\n" ),
 	 _( "Joachim Metz" ) );
 
 	fprintf(
