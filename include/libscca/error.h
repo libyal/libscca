9e01daf46d825d40aea539c613fdbeaae9b4e8f4
Joachim Metz <joachim.metz@gmail.com>
2026-06-21 13:00:06 +0200

    Worked on tests and changes for CI tests

--- include/libscca/error.h
+++ include/libscca/error.h
@@ -82,7 +82,7 @@ enum LIBSCCA_ARGUMENT_ERROR
 	 */
 	LIBSCCA_ARGUMENT_ERROR_UNSUPPORTED_VALUE	= 8,
 
-	/* The argument contains a value that conficts with another argument
+	/* The argument contains a value that conflicts with another argument
 	 */
 	LIBSCCA_ARGUMENT_ERROR_CONFLICTING_VALUE	= 9
 };
@@ -214,7 +214,7 @@ enum LIBSCCA_OUTPUT_ERROR
 {
 	LIBSCCA_OUTPUT_ERROR_GENERIC			= 0,
 
-	/* There is insuficient space to write the output
+	/* There is insufficient space to write the output
 	 */
 	LIBSCCA_OUTPUT_ERROR_INSUFFICIENT_SPACE		= 1
 };
